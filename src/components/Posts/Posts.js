22d8b89b0f80417d5a429cbffcba1cc394ed3f3e
spettigrew <sara.pettigrew5@gmail.com>
2021-01-21 17:06:26 -0700

    Add comments to post added

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,11 +4,21 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { likePost, posts, addComment, text} = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(post => {
+        return (
+          <Post 
+          key={post.id} 
+          post={post} 
+          likePost={likePost}
+          addComment={addComment}
+          text={text}/>
+        )
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
