4bf1bc3dbea1fddf5db1a4e6aa66ad48e2a515fc
spettigrew <sara.pettigrew5@gmail.com>
2021-01-21 17:05:03 -0700

    Stretch of adding comments added

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -8,7 +8,7 @@ import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
-  const { likePost, numberOfLikes } = props;
+  const { likePost, numberOfLikes, addComment, text, handleCommentChange, post } = props;
 
   return (
     <div>
@@ -17,13 +17,17 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={() => addComment(post.id)} likePost={likePost}/>
         </div>
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faComment} />
+          <FontAwesomeIcon icon={faComment}/>
+        </div>
+        <div className='like-section-wrapper'>
+          <input value={text} onChange={handleCommentChange} />
+          <button onClick={addComment}>Comment</button>
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes}</p>
     </div>
   );
 };
